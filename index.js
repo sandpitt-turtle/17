021c66a0fab25ea74e6af8fcf39d185acabe12ce
sandpitt-turtle <wottkowicz@gmail.com>
2025-01-07 08:41:03 -0500

    needed to finish somehow. losing it

--- index.js
+++ index.js
@@ -10,28 +10,45 @@ class Numbers {
     }
   }
   count() {
-    //return the count of numbers in data
+    // Return the count of numbers in data
+    return this.data.length;
   }
+
   printNumbers() {
-    //print the numbers in data
+    // Print the numbers in data along with their indexes
+    this.data.forEach((num, index) => {
+      console.log(`Index: ${index}, Number: ${num}`);
+    });
   }
-  odds() {
-    //return the odd numbers in data
+
+   odds() {
+    // Return the odd numbers in data
+    return this.data.filter((num) => num % 2 !== 0);
   }
+
   evens() {
-    //return the even numbers in data
+    // Return the even numbers in data
+    return this.data.filter((num) => num % 2 === 0);
   }
+
   sum() {
-    //return the sum of the numbers
+    // Return the sum of the numbers
+    return this.data.reduce((acc, num) => acc + num, 0);
   }
+
   product() {
-    //return the product of the numbers
+    // Return the product of the numbers
+    return this.data.reduce((acc, num) => acc * num, 1);
   }
+
   greaterThan(target) {
-    //return the numbers greater than the target
+    // Return the numbers greater than the target
+    return this.data.filter((num) => num > target);
   }
+
   howMany(target) {
-    //return the count of a given number
+    // Return the count of a given number
+//i do not know.
   }
 }
 
@@ -40,6 +57,7 @@ const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
 
 //create an instance of numbers
 const n1 = new Numbers(str);
+
 console.log(n1.count()); //returns count of numbers
 n1.printNumbers(); //prints the number along with their indexes
 console.log(n1.odds()); //returns odd numbers
